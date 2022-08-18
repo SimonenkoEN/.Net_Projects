f15e7d6a454f7e3d56cda5d4cabb043eb1d286b3
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-08-18 22:28:29 +0300

    Add Proj004

---
+++ Proj004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negnum = num * -1;
+
+while (negnum <= num)
+{
+    Console.Write(negnum);
+    negnum++;
+}
