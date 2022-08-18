8a025ead394497048fee8f7eca03f5605c775aaa
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-08-18 20:28:28 +0300

    нахождение sqr

--- Proj001/Program.cs
+++ Proj001/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number * number;
+Console.Write("Квадрат числа = ");
+Console.WriteLine(sqr);
