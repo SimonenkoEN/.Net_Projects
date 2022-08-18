b0f60dd48ed1ce9171470432f639fc63454ba90d
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-08-18 20:56:27 +0300

    Add Proj002

---
+++ Proj002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Add numbrt1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Add numbrt2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+
+    Console.WriteLine("Число 1 является квадратом числа 2!");
+}
+else
+{
+    Console.WriteLine("Число 1 не является квадратом числа 2!");
+}
