29505d9d3cc917a4755d471f180381aace1539ec
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-08-22 21:36:09 +0300

    -

---
+++ Proj006/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+
+int firstNum = num / 10;
+int secondNum = num % 10;
+
+if(firstNum > secondNum){
+    Console.WriteLine("Максимальное число: {0}", firstNum);
+}
+else{
+    Console.WriteLine("Максимальное число: {0}", secondNum);
+}
---
+++ Proj007/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int res = (num / 100) * 10 + (num % 10);
+
+Console.WriteLine(res);
