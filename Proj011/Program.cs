010770756c9bbb1153763ac166c095ff5232bc37
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-09-15 21:45:14 +0300

    Add 2 methods

---
+++ Proj011/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int m = 8;
+int n = 5;
+Console.WriteLine(GetPower(m, n));
+Console.WriteLine(GetSum(GetPower(m, n)));
+
+int GetPower(int a, int b, int res = 1, int i = 1)
+{
+    if (i <= b)
+    {
+        res *= a;
+        return GetPower(a, b, res, i + 1);
+    }
+    return res;
+}
+
+int GetSum(int a, int res = 0)
+{
+    if (a != 0)
+    {
+        res += a % 10;
+        return GetSum(a / 10, res);
+    }
+    return res;
+}
